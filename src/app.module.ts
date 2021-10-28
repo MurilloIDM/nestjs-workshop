e6d64570c4b94fc8c3ad738874718dca9dfb12ad
MurilloIDM <murillo.idem@gmail.com>
2021-10-28 06:41:22 -0300

    New: Create module to prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './modules/Prisma/prisma.module';
 import { UserModule } from './modules/User/user.module';
 
 @Module({
   imports: [
-    UserModule
+    PrismaModule,
+    UserModule,
   ]
 })
 
